ff6e1dc5b4a5698e8111cb1e35bffb63fee06e47
sbenateau <sbenateau@mailodie.fr>
2023-02-28 10:52:55 +0100

    make menu 2 columns

--- modules/mod_navigation.R
+++ modules/mod_navigation.R
@@ -8,17 +8,21 @@ mod_navigation_ui <- function(id) {
     column(width = 6, offset = 4, align="left",
            rep_br(1),
            p("Vous pouvez continuer en important un nouveau jeu de donnée, en manipulant les données ou en les visualisant"),
-           img(src = "picto/Importer.png", height = "30px"), actionButton(ns("import_nav"), "Importer d'autres données"),
-           br(), br(),
-           img(src = "picto/Manipuler.png", height = "30px"), actionButton(ns("manip_nav"), "Manipuler les données"),
-           br(), br(),
-           img(src = "picto/Visualiser.png", height = "30px"), actionButton(ns("visu_nav"), "Visualiser les données"),
-           br(), br(),
-           img(src = "picto/Tester.png", height = "30px"), actionButton(ns("stat_navt"), "Effectuer un test statistique"),
-           br(), br(),
-           img(src = "picto/Conclusion.png", height = "30px"), actionButton(ns("conclusion_nav"), "Conclure"),
-           br(), br(),
-           img(src = "picto/Exporter.png", height = "30px"), actionButton(ns("rapport_nav"), "Éditer un rapport")
+           column(width = 6,
+                  img(src = "picto/Importer.png", height = "30px"), actionButton(ns("import_nav"), "Importer d'autres données"),
+                  br(), br(),
+                  img(src = "picto/Manipuler.png", height = "30px"), actionButton(ns("manip_nav"), "Manipuler les données"),
+                  br(), br(),
+                  img(src = "picto/Visualiser.png", height = "30px"), actionButton(ns("visu_nav"), "Visualiser les données"),
+                  br(), br(),
+           ),
+           column(width = 6,
+                  img(src = "picto/Tester.png", height = "30px"), actionButton(ns("stat_navt"), "Effectuer un test statistique"),
+                  br(), br(),
+                  img(src = "picto/Conclusion.png", height = "30px"), actionButton(ns("conclusion_nav"), "Conclure"),
+                  br(), br(),
+                  img(src = "picto/Exporter.png", height = "30px"), actionButton(ns("rapport_nav"), "Éditer un rapport")
+           )
     )
   )
 }
@@ -27,35 +31,35 @@ mod_navigation_server <- function(id, parent_session){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
 
-      observeEvent(input$import_nav, {
-        updateTabsetPanel(parent_session, "vigie_nature_analyse",
-                          selected = "import")
-      })
+    observeEvent(input$import_nav, {
+      updateTabsetPanel(parent_session, "vigie_nature_analyse",
+                        selected = "import")
+    })
 
-      observeEvent(input$manip_nav, {
-        updateTabsetPanel(parent_session, "vigie_nature_analyse",
-                          selected = "manip")
-      })
+    observeEvent(input$manip_nav, {
+      updateTabsetPanel(parent_session, "vigie_nature_analyse",
+                        selected = "manip")
+    })
 
-      observeEvent(input$visu_nav, {
-        updateTabsetPanel(parent_session, "vigie_nature_analyse",
-                          selected = "visualisation")
-      })
+    observeEvent(input$visu_nav, {
+      updateTabsetPanel(parent_session, "vigie_nature_analyse",
+                        selected = "visualisation")
+    })
 
-      observeEvent(input$stat_navt, {
-        updateTabsetPanel(parent_session, "vigie_nature_analyse",
-                          selected = "stats")
-      })
+    observeEvent(input$stat_navt, {
+      updateTabsetPanel(parent_session, "vigie_nature_analyse",
+                        selected = "stats")
+    })
 
-      observeEvent(input$conclusion_nav, {
-        updateTabsetPanel(parent_session, "vigie_nature_analyse",
-                          selected = "conclusion")
-      })
+    observeEvent(input$conclusion_nav, {
+      updateTabsetPanel(parent_session, "vigie_nature_analyse",
+                        selected = "conclusion")
+    })
 
-      observeEvent(input$rapport_nav, {
-        updateTabsetPanel(parent_session, "vigie_nature_analyse",
-                          selected = "rapport")
-      })
+    observeEvent(input$rapport_nav, {
+      updateTabsetPanel(parent_session, "vigie_nature_analyse",
+                        selected = "rapport")
+    })
 
   })
 }
